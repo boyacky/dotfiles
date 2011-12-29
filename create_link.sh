9bd823f938e2cb362d7031dfcc4f28c6753602d8
Keiitiro Yamaoka <boyacky.mail@gmail.com>
2011-12-29 16:47:34 +0900

    少し調整

--- create_link.sh
+++ create_link.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd $(dirname $0)
-( ls -1 -A | egrep '^\.' | egrep -v '^(.git|.gitmodules|.gitignore)$' | while read dotfile; do
+( ls -1 -A | egrep '^\.' | egrep -v '^(.git|.gitmodules|.gitignore|.vimrcs)$' | while read dotfile; do
 #    echo "if [ ! -e $HOME/$dotfile ]; then ln -fs \"$PWD/$dotfile\" $HOME; fi"
     echo "ln -fs \"$PWD/$dotfile\" $HOME"
 done ) | sh > /dev/null
